5bcf0646eae2d0320b4741b069c778cfd9803356
baronchibuikem <baronchibuike@gmail.com>
2021-04-03 12:03:40 +0100

    update: change the url to the home directory

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from home.views import home, blog_detail, about
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('profile/', home, name='home'),
     path("post/<str:slug>/", blog_detail, name="post-detail"),
-    path("profile/", about, name="about")
+    path("", about, name="about")
 ]
